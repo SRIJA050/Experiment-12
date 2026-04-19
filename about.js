b4327e348007721fb8baca3cdd79d709f82201b1
SRIJA050 <asrija1702@gmail.com>
2026-04-19 11:49:40 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h2>About This App</h2>
+      <p>This is a Student Management System built using React and Routing.</p>
+    </div>
+  );
+};
+
+export default About;
